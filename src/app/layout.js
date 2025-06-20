df9a78f4aaed36e37f0d7833d1cc8b670b93751e
shreyanithin <shreyanithinr@gmail.com>
2025-06-21 02:32:18 +0530

    logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+         <link rel="icon" href="/logo.jpeg" type="image/jpeg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
