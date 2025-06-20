79b56690c100a7fcfbdd9fde087a6cf799751174
shreyanithin <shreyanithinr@gmail.com>
2025-06-21 00:19:42 +0530

    scroll issue corrected

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -16,7 +16,7 @@ export default function Header(){
 
     return(
         <header
-            className={`fixed top-0 left-0 w-full bg-black text-white transition-transform duration-500 ${showHeader ? "translate-y-0" : "-translate-y-full"}`}
+            className={`fixed top-0 left-0 w-full z-50 bg-black text-white transition-transform duration-500 ${showHeader ? "translate-y-0" : "-translate-y-full"}`}
         >
             <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
                 <h1 className="text-xl font-bold text-[#e62b1e]">TEDxCUSAT 2025</h1>
