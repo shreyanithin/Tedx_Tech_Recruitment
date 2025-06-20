3162af9fc53584888d7ca7389bf3a44905ef341e
shreyanithin <shreyanithinr@gmail.com>
2025-06-21 02:47:45 +0530

    hamburger menu set

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,9 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import {Menu, X} from "lucide-react";
 
 export default function Header(){
     const[showHeader, setShowHeader] = useState(false);
+    const[menuOpen,setMenuOpen]= useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -20,7 +22,8 @@ export default function Header(){
         >
             <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
                 <h1 className="text-xl font-bold text-[#e62b1e]">TEDxCUSAT 2025</h1>
-                <nav className="space-x-6 text-sm md:text-base">
+
+                <nav className="hidden md:flex space-x-6 text-sm md:text-base">
                     <a href="#about" className="hover:text-[#e62b1e] transition">
                         About
                     </a>
@@ -31,7 +34,42 @@ export default function Header(){
                         Sponsors
                     </a>
                 </nav>
+
+
+                <button
+                    className="md:hidden z-50"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                >
+                    {menuOpen ? <X size={28} /> : <Menu size={28} />}
+                </button>
             </div>
+        <div
+        className={`md:hidden bg-black text-white absolute top-full left-0 w-full px-6 py-4 space-y-4 transition-all duration-300 ${
+          menuOpen ? "block" : "hidden"
+        }`}
+      >
+        <a
+          href="#about"
+          className="block hover:text-[#e62b1e]"
+          onClick={() => setMenuOpen(false)}
+        >
+          About
+        </a>
+        <a
+          href="#speakers"
+          className="block hover:text-[#e62b1e]"
+          onClick={() => setMenuOpen(false)}
+        >
+          Speakers
+        </a>
+        <a
+          href="#sponsors"
+          className="block hover:text-[#e62b1e]"
+          onClick={() => setMenuOpen(false)}
+        >
+          Sponsors
+        </a>
+      </div>    
         </header>
     );
 }
