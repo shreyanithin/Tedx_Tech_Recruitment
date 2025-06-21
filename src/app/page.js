8cf877520026c06fac7597e880ba44b6f3e0afa8
shreyanithin <shreyanithinr@gmail.com>
2025-06-21 15:14:06 +0530

    name error

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Hero from "./components/Hero"
 import Header from "./components/Header";
 import Countdown from "./components/Countdown";
 import About from "./components/About";
-import Stats from "./components/stats"
+import Stats from "./components/Stats"
 import Speakers from "./components/Speakers";
 import Sponsors from "./components/Sponsors";
 import Footer from "./components/Footer";
